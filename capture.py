dd76bcd2c1d6026482afb4701521ebf7c34bc00c
iris-recognition <47550188+iris-recognition@users.noreply.github.com>
2019-03-01 15:41:44 +0530

    Update capture.py

--- capture.py
+++ capture.py
@@ -14,11 +14,12 @@ while True:
     k = cv2.waitKey(1)
 
     if k%256 == 27:
+       
         print("Escape hit, closing...")
         break
     elif k%256 == 32:
-        
-        img_name = "opencv_frame_{}.png".format(img_counter)
+       
+        img_name = "{}.jpeg".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
